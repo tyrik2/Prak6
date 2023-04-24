a0bfcb7655b27a9d626cf2fac1ce24c15f203e99
Daniel <vfgkc3@gmail.com>
2023-04-24 23:50:29 +0300

    Готовое задание 1

--- Prak6/MainWindow.xaml.cs
+++ Prak6/MainWindow.xaml.cs
@@ -15,14 +15,32 @@ using System.Windows.Shapes;
 
 namespace Prak6
 {
+    
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int f(int x)
+        {
+            return x % 10;
+        }
+
+        // Метод для вычисления значения z = f(a) + f(b)
+        public int ComputeZ(int a, int b)
+        {
+            return f(a) + f(b);
+        }
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int a = int.Parse(txtA.Text);
+            int b = int.Parse(txtB.Text);
+            int z = ComputeZ(a, b);
+            txtZ.Text = z.ToString();
         }
     }
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prak6;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestF
+    {
+        [TestMethod]
+        public void TestF1()
+        {
+            int x = 123;
+            int expected = 3;
+            int actual = MainWindow.f(x);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestF2()
+        {
+            int x = 456;
+            int expected = 6;
+            int actual = expected.f(x);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestF3()
+        {
+            int x = 789;
+            int expected = 9;
+            int actual = MainWindow.f(x);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
